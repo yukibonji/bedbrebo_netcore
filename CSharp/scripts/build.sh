7f859301f734cbf10d6a78b8f4895ba8e565d414
Jaskula Tomasz <thomasv1000@hotmail.fr>
2016-07-09 23:12:41 +0200

    deleting "dotnet" keyword from build.sh

--- CSharp/scripts/build.sh
+++ CSharp/scripts/build.sh
@@ -22,20 +22,20 @@ then
   [ ! -e build.fsx ] && packages/FAKE/tools/FAKE.exe ../scripts/init.fsx
   packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO ../scripts/build.fsx 
 else
-  # use mono
-  dotnet .paket/paket.bootstrapper.exe
+  # use dotnet core
+  .paket/paket.bootstrapper.exe
   exit_code=$?
   if [ $exit_code -ne 0 ]; then
   	exit $exit_code
   fi
 
-  dotnet .paket/paket.exe restore
+  .paket/paket.exe restore
   exit_code=$?
   if [ $exit_code -ne 0 ]; then
   	exit $exit_code
   fi
 
-  [ ! -e build.fsx ] && dotnet .paket/paket.exe update
-  [ ! -e build.fsx ] && dotnet packages/FAKE/tools/FAKE.exe ../scripts/init.fsx
-  dotnet packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO ../scripts/build.fsx 
+  [ ! -e build.fsx ] && .paket/paket.exe update
+  [ ! -e build.fsx ] && packages/FAKE/tools/FAKE.exe ../scripts/init.fsx
+  packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO ../scripts/build.fsx 
 fi
